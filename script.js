03d439003af7347422165f4ec06477cd776575cc
Abhijeet Kumar <145859677+abhijeet8340@users.noreply.github.com>
2025-03-18 03:19:03 +0530

    Add files via upload
    This is a personal portfolio website built using HTML, CSS, and JavaScript. It showcases my skills, projects, and contact details in a clean and responsive design

---
+++ script.js
@@ -0,0 +1,41 @@
+
+var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+        function opentab(tabname){
+            for(tablink of tablinks){
+                tablink.classList.remove("active-link");
+            }
+            for(tabcontent of tabcontents){
+                tabcontent.classList.remove("active-tab");
+            }
+            event.currentTarget.classList.add("active-link");
+            document.getElementById(tabname).classList.add("active-tab")
+        }
+
+
+        var sidemeu = document.getElementById("sidemenu");
+
+        function openmenu(){
+            sidemeu.style.right = "0";
+        }
+        function closemenu(){
+            sidemeu.style.right = "-200px";
+        }
+
+
+
+ 
+
+// Intersection Observer for scrolling animations
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        }
+    });
+});
+
+// Apply animation to sections
+document.querySelectorAll('.container').forEach((section) => {
+    observer.observe(section);
+});
